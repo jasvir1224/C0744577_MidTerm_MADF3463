321a075ffd20a9ebf7e521837d5b84fcc8c0fdbf
jasvirkaur <jasvir.kaur1224@gmail.com>
2018-10-19 21:07:18 -0400

    second init

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,9 +1,49 @@
 package com.lambton;
 
+
 public class LambtonStringTools {
 
 
-    String s = "Lambton";
-    
+    String reversestring(String s) {
+        String string = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            string = string + s.charAt(i);
+
+        }
+
+        System.out.println("Original input is:" + s);
+
+        System.out.println("After Execution Output is: " + string);
+        return s;
+    }
+
+
+    int binarytodecimal(int s) {
+
+        int d = 0;
+        int n = 0;
+
+        int a = s % 10;
+        d = a * 2;
+        int i = s / 10;
+
+        System.out.println(d);
+        return d;
+
+    }
+
+    String replacesubstring(String s1, String s2, String s3) {
+
+        s1 = "the dog jumbed over the fence";
+        s2 = "that";
+      s3
+
+        return s1;
+
+    }
+
+
 
 }
---
+++ src/main/java/com/lambton/Testmain.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class Testmain {
+
+    public static void main(String[] args) {
+
+      LambtonStringTools l = new LambtonStringTools();
+     l.reversestring("Lambton");
+
+     l.binarytodecimal(1000);
+
+    }
+
+}
